4f1a6cf0d68efa7dd101dde29a11344d1c603cb4
Ravindu Deshan <Droidcasts@protonmail.com>
2025-06-24 14:18:30 +0530

    build.sh: Update OS patch level

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ export GKI_KERNEL_BUILD_OPTIONS="
 # Build options (extra)
 export MKBOOTIMG_EXTRA_ARGS="
     --os_version 13.0.0 \
-    --os_patch_level 2025-01-00 \
+    --os_patch_level 2025-04-00 \
     --pagesize 4096
 "
 
