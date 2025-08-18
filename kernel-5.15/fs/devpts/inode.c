ef35dd759f463ab56de6ca0cc8fe57c9bd97dc9c
ravindu644 <droidcasts@protonmail.com>
2025-08-18 23:16:58 +0530

    kernelsu: fix transaction errors in `am`, `pm` commands
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- kernel-5.15/fs/devpts/inode.c
+++ kernel-5.15/fs/devpts/inode.c
@@ -599,6 +599,10 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_devpts(struct inode*);
+#endif
+
 #if defined(CONFIG_KSU_SUSFS_SUS_SU)
 extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
@@ -612,6 +616,11 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
+
+#ifdef CONFIG_KSU
+ksu_handle_devpts(dentry->d_inode);
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 	if (likely(susfs_is_current_proc_su_not_allowed())) {
 		goto orig_flow;
