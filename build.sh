b16d4d1b9ae918331ccdb92a6ed6ea392b06ac3a
ravindu644 <droidcasts@protonmail.com>
2026-07-07 12:14:16 +0530

    build: introduced a simple build script to build the stock GKI kernel
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname $(readlink -fq $0))"
+
+# init & update git submodules
+git submodule update --init --recursive || true
+
+# download & install Samsung's ndk
+if [[ ! -d "${SCRIPT_DIR}/kernel/prebuilts" || ! -d "${SCRIPT_DIR}/prebuilts" ]]; then
+    echo -e "Cloning Samsung's NDK..."
+    curl -LO "https://github.com/Kernels-by-ravindu644/samsung_kernel_a166p/releases/download/toolchain/toolchain.tar.gz" || {
+        echo "Failed to download Samsung's NDK. Please check your internet connection and try again." && exit 1
+    }
+    tar -xf toolchain.tar.gz && rm toolchain.tar.gz
+fi
+
+# cleanup before building
+rm -rf "${SCRIPT_DIR}/out"
+
+# generate the build.config
+cd "${SCRIPT_DIR}/kernel-5.15" && \
+    python scripts/gen_build_config.py \
+        --kernel-defconfig a16xm_00_defconfig \
+        --kernel-defconfig-overlays "entry_level.config S98901AA1.config S98901AA1_debug.config" \
+        -m user \
+        -o ../out/target/product/a16xm/obj/KERNEL_OBJ/build.config && \
+    cd "${SCRIPT_DIR}"
+
+# export environment variables from the samsung's build_kernel.sh
+export ARCH=arm64
+export PLATFORM_VERSION=13
+export CROSS_COMPILE="aarch64-linux-gnu-"
+export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
+export OUT_DIR="../out/target/product/a16xm/obj/KERNEL_OBJ"
+export DIST_DIR="../out/target/product/a16xm/obj/KERNEL_OBJ"
+export BUILD_CONFIG="../out/target/product/a16xm/obj/KERNEL_OBJ/build.config"
+
+# build the kernel
+build_kernel(){
+    cd "${SCRIPT_DIR}/kernel"
+
+    ./build/build.sh
+
+    cd "${SCRIPT_DIR}"
+}
+
+build_kernel || exit 1
