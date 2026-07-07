2bd48d34bb9cb061cb040c9b59a425d7f125ec36
ravindu644 <droidcasts@protonmail.com>
2026-07-07 13:52:27 +0530

    build script: build the kernel with thin-LTO
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -38,6 +38,7 @@ export BUILD_CONFIG="../out/target/product/a16xm/obj/KERNEL_OBJ/build.config"
 # add custom build options to here
 # checkout kernel/build/build.sh to possible variables
 GKI_KERNEL_BUILD_OPTIONS=(
+    "LTO=thin"
     "SKIP_MRPROPER=1"
     "KMI_SYMBOL_LIST_STRICT_MODE=0"
     "ABI_DEFINITION="
